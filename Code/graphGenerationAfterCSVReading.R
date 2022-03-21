f36c44d5cdd680935992cafd00d656e7722fb06c
Lakshay Rastogi <lakshyarastogi31@gmail.com>
2022-03-21 16:36:11 +0530

    Report Code and Graph Generation Code Added

---
+++ Code/graphGenerationAfterCSVReading.R
@@ -0,0 +1,42 @@
+data <- read.csv("/mnt/d/2022_Jan-Apr/Semester 8/Mth UGP/Code/StatisticCSVFIle/phi_0_6.csv")
+print(data)
+
+vals <- matrix(0, nrow = 8, ncol = 9)
+# vals[2,] <- as.numeric(data[2,])
+phi_val = c(6,9,95,99)
+
+
+for(x in 0:(length(phi_val)-1)){
+    data <- read.csv(paste0("/mnt/d/2022_Jan-Apr/Semester 8/Mth UGP/Code/StatisticCSVFIle/phi_0_",phi_val[x+1],".csv"))
+    for(y in 1:2){
+        vals[(2*x) + y,] <- as.numeric(data[y,])
+    }
+}
+
+pdf(file = paste("/mnt/d/2022_Jan-Apr/Semester 8/Mth UGP/Code/Report_Images/Stats_1.pdf",sep=""), height = 10, width = 8, pointsize = 10)
+
+name <- c("Mean","Variance","MSE");
+par(oma = c(4,1,1,1), mfrow = c(4, 3), mar = c(2, 2, 1, 1))
+for(i in seq.int(0,7,2)){
+    curr_data <- t(vals[(i+1):(i+2),])
+    for(j in 0:2){
+        plot_data <- curr_data[((j*3)+1):((j*3)+3),]
+        barplot(plot_data, main = paste0(name[j+1], " Phi : 0.",phi_val[(i/2)+1]), ylim = range(pretty(c(0,plot_data))), col = c("red","blue","green"), names.arg = c(b_dist[1],b_dist[2]),beside = TRUE)
+    }
+}
+par(fig = c(0, 1, 0, 1), oma = c(0, 0, 0, 0), mar = c(1, 1, 1, 1), new = TRUE)
+plot(10, type = "n", axes=FALSE, xlab="", ylab="")
+plot_colors <- c("red","blue", "green")
+legend(x = "bottom",inset = 0,
+        legend = c("BM ","Estimated ", "Estimated AIC "), 
+        col=plot_colors, lwd=5, cex=.8, horiz = TRUE)
+
+dev.off()
+
+curr_data <- vals[1:2,]
+t_curr_data <- t(curr_data)
+# reshape_data <- reshape(curr_data, )
+broken_data <- t_curr_data[1:3,]
+
+barplot(broken_data, main = "Mean", ylim = range(pretty(c(0,broken_data))), col = c("red","blue","green"), names.arg = c(b_dist[1],b_dist[2]),beside = TRUE)
+legend("topright", legend=c("BM ","Estimated ", "Estimated AIC "), col=c("red","blue", "green"), lty=1:1:1, cex=0.75)
--- Code/varOfEstimate_AR_BatchSize.R
+++ Code/varOfEstimate_AR_BatchSize.R
@@ -6,8 +6,18 @@ calculate_est <- function(b, alpha_sq, rho) {
 
 # b_dist <- c(100,200,500,1000);
 # b_dist <- c(10,20,50,100);
+
 b_dist <- numeric(2);
 iter <- 100
+n_phi = 4
+#for plotting everything on the same graph
+bm_sigma_dist_phi <- matrix(0, nrow = n_phi*length(b_dist), ncol = iter)
+est_sigma_dist_phi <- matrix(0, nrow = n_phi*length(b_dist), ncol = iter)
+est_sigma_dist_aic_phi <- matrix(0, nrow = n_phi*length(b_dist), ncol = iter)
+
+rho_dist_phi <- matrix(0, nrow = n_phi*length(b_dist), ncol = iter)
+rho_dist_aic_phi <- matrix(0, nrow = n_phi*length(b_dist), ncol = iter)
+
 
 sample_mean_dist <- numeric(length = iter)
 
@@ -21,6 +31,7 @@ est_sigma_dist_aic <- matrix(0, nrow = length(b_dist), ncol = iter)
 rho_dist_aic <- matrix(0,nrow = length(b_dist), ncol = iter)
 alpha_dist_aic <- matrix(0,nrow = length(b_dist), ncol = iter)
 
+
 T <- 1e5
 b_dist[2] <- floor(T^(1/2))
 b_dist[1] <- floor(T^(1/3))
@@ -28,7 +39,14 @@ b_dist[1] <- floor(T^(1/3))
 phi <- 0.99
 alpha <- 1
 
-sigma_true = (alpha^2)/((1-phi)^2)
+
+phi_dist = c(0.6,0.9,0.95,0.99)
+
+sigma_true <- numeric(4)
+for(i in 1:length(phi_dist)){
+    sigma_true[i] <- (alpha^2)/((1-phi_dist[i])^2)
+}
+
 
 # This is to the code for which model has to be made for an AR1 process
 for(it in 1:iter){
@@ -91,36 +109,75 @@ for(it in 1:iter){
 cbind(apply(bm_sigma_dist,1,mean),apply(est_sigma_dist,1,mean),apply(est_sigma_dist_aic,1,mean))
 cbind(apply(bm_sigma_dist,1,var),apply(est_sigma_dist,1,var),apply(est_sigma_dist_aic,1,var)) 
 
+mean_var = cbind(apply(bm_sigma_dist,1,mean),apply(est_sigma_dist,1,mean),apply(est_sigma_dist_aic,1,mean))
+var_var = cbind(apply(bm_sigma_dist,1,var),apply(est_sigma_dist,1,var),apply(est_sigma_dist_aic,1,var)) 
+
+mse_var <- matrix(0, nrow = length(b_dist), ncol = 3)
 for(i in 1:length(b_dist)){
     mse_bm = (1/n)*sum((sigma_true-bm_sigma_dist[i,])^2);
     mse_est = (1/n)*sum((sigma_true-est_sigma_dist[i,])^2);
     mse_est_aic = (1/n)*sum((sigma_true-est_sigma_dist_aic[i,])^2);
+    # mse_var[i,] = formatC(c(mse_bm, mse_est, mse_est_aic), format="e", digits = 4)
+    mse_var[i,] = c(mse_bm, mse_est, mse_est_aic);
     print(paste("Batch Size ",b_dist[i]," MSE BM: ",mse_bm," MSE EST: ",mse_est,"MSE AIC EST: ", mse_est_aic));
 }
 
+data <- data.frame("mean"=mean_var, "var"=var_var, "mse"=mse_var)
+rownames(data) = c(b_dist[1],b_dist[2])
+data
+write.csv(data, file = paste("/mnt/d/2022_Jan-Apr/Semester 8/Mth UGP/Code/StatisticCSVFIle/phi_",phi))
+# 0.6, 0.9. 0.95, 0.99
 
-pdf(file = paste("/mnt/d/2022_Jan-Apr/Semester 8/Mth UGP/Latec Images/Week 8/",phi,"_AR.pdf",sep=""), height = 8, width = 10, pointsize = 10)
-par(mfrow=c(2,1))    
+x = 6
+for(i in 1:length(b_dist)){
+    print(x + i)
+    bm_sigma_dist_phi[x + i, ] <- bm_sigma_dist[i,]
+    est_sigma_dist_phi[x + i, ] <- est_sigma_dist[i,]
+    est_sigma_dist_aic_phi[x + i,]  <- est_sigma_dist_aic[i,]
 
-for(i in 1:length(b_dist)) {
-    min_lim = min(bm_sigma_dist[i,],est_sigma_dist[i,],est_sigma_dist_aic[i,])
-    max_lim = max(bm_sigma_dist[i,],est_sigma_dist[i,],est_sigma_dist_aic[i,])
-    range = max_lim - min_lim
-    plot(density(bm_sigma_dist[i,]), xlim = c(min_lim - 0.4*(range), max_lim + 0.4*(range)), main = paste("Batch Size : ",b_dist[i]), lwd = 1, col = "red")
-    abline(v = sigma_true)
-    lines(density(est_sigma_dist[i,]), lwd = 1, col = "blue")
-    lines(density(est_sigma_dist_aic[i,]), lwd = 1, col ="green")
-    legend("topright", legend=c("BM ","Estimated ", "Estimated AIC "), col=c("red","blue", "green"), lty=1:1:1, cex=0.8)
+    rho_dist_phi[x + i, ] <- rho_dist[i,]
+    rho_dist_aic_phi[x + i, ] <- rho_dist_aic[i,]
+}
+
+
+pdf(file = paste("/mnt/d/2022_Jan-Apr/Semester 8/Mth UGP/Code/Report_Images/Phi_AR_1.pdf",sep=""), height = 10, width = 8, pointsize = 10)
+
+
+par(mfrow=c(4,2))   
+
+
+for(x in seq.int(0,7,2)){
+    for(i in 1:length(b_dist)) {
+        bm_sigma_dist[i,] = bm_sigma_dist_phi[x+i,];
+        est_sigma_dist[i,] = est_sigma_dist_phi[x+i,];
+        est_sigma_dist_aic[i,] = est_sigma_dist_aic_phi[x+i,];
+        min_lim = min(bm_sigma_dist[i,],est_sigma_dist[i,],est_sigma_dist_aic[i,])
+        max_lim = max(bm_sigma_dist[i,],est_sigma_dist[i,],est_sigma_dist_aic[i,])
+        range = max_lim - min_lim
+        plot(density(bm_sigma_dist[i,]), xlim = c(min_lim - 0.5*(range), max_lim + 0.5*(range)), main = paste("Phi : ", phi_dist[x/2 + 1],"Batch Size : ",b_dist[i]), lwd = 1, col = "red")
+        abline(v = sigma_true[x/2 + 1])
+        lines(density(est_sigma_dist[i,]), lwd = 1, col = "blue")
+        lines(density(est_sigma_dist_aic[i,]), lwd = 1, col ="green")
+        legend("topright", legend=c("BM ","Estimated ", "Estimated AIC "), col=c("red","blue", "green"), lty=1:1:1, cex=0.8)
+    }
 }
 
 dev.off()
 
-pdf(file = paste("/mnt/d/2022_Jan-Apr/Semester 8/Mth UGP/Latec Images/Week 8/",rho,"_RHO.pdf",sep=""), height = 8, width = 10, pointsize = 10)
-par(mfrow=c(2,1))    
+pdf(file = paste("/mnt/d/2022_Jan-Apr/Semester 8/Mth UGP/Code/Report_Images/Rho_2.pdf",sep=""), height = 10, width = 8, pointsize = 10)
 
-for(i in 1:length(b_dist)) {
-    plot(density(rho_dist[i,]), col = "red", main = paste("Rho ", mean(rho_dist[i,]),"BS : ", b_dist[i]))
-    abline(v = mean(rho_dist[i,]))
+par(mfrow=c(4,2))    
+for( x in seq.int(0,7,2)){
+    for(i in 1:length(b_dist)){
+        rho_dist[i, ] = rho_dist_phi[x+i, ]
+        rho_dist_aic[i, ] = rho_dist_aic_phi[x+i, ]
+        min_x = min(min(density(rho_dist[i, ])$x), max(density(rho_dist_aic[i, ])$x))
+        max_x = max(max(density(rho_dist[i, ])$x), max(density(rho_dist_aic[i, ])$x))
+        plot(density(rho_dist_aic[i,]), col = "blue")
+        # plot(density(rho_dist_aic[i,]), ylim = c(0,max(max(density(rho_dist[i, ])$y), max(density(rho_dist_aic[i, ])$y))), xlim = c(min_x, max_x),  col = "blue", main = paste("BS : ", b_dist[i])) # "Rho ", mean(rho_dist[i,]) ,"AIC Rho ", mean(rho_dist_aic[i,])
+        # lines(density(rho_dist[i,]), lwd = 1, col = "red")
+        legend("topright", legend=c("AIC = FALSE ","AIC = TRUE"), col=c("red","blue"), lty=1:1, cex=0.8)
+    }
 }
 
 dev.off()
